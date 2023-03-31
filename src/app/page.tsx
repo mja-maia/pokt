1a886bbcf0a962a2664381a9c9c897537cf8a3e7
Miguel Maia <miguel@cupdev.com.br>
2023-03-31 17:12:38 -0300

    feat: initial setup using next and tailwind

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,91 +1,52 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
-import styles from './page.module.css'
+"use client";
+import { useEffect, useState } from "react";
 
-const inter = Inter({ subsets: ['latin'] })
+interface Users {
+  username: string;
+  firstname: string;
+  email: string;
+  lastname: string;
+  image: string;
+  id: string;
+  website: string;
+}
 
 export default function Home() {
+  const [users, setUsers] = useState<Users[] | null>(null);
+
+  useEffect(() => {
+    const users = fetch("https://fakerapi.it/api/v1/users")
+      .then((response) => response.json())
+      .then(({ status, data }) => {
+        if (status === "OK") {
+          setUsers(data);
+        }
+      });
+  }, []);
+
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
+    <div className="h-full p-4 flex flex-col items-center justify-center">
+      <div className="text-center text-3xl text-white mb-5">Users</div>
+
+      <div className="grid  lg:grid-cols-3 md:grid-cols-2 sm:grid-cols-1 gap-4">
+        {users?.map(({ username, id, email, firstname, lastname, website }) => (
+          <div className="flex flex-col w-96 bg-white p-4  rounded-md" key={id}>
+            <div className="flex items-center">
+              <div className="w-16 h-16 rounded-full bg-slate-200 flex items-center justify-center mr-4">
+                Avatar
+              </div>
+              <div>{username}</div>
+            </div>
+            <div className="mt-2 flex flex-col">
+              <div>
+                {firstname} {lastname}
+              </div>
+              <div>{email}</div>
+              <div>{website}</div>
+            </div>
+          </div>
+        ))}
       </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-        <div className={styles.thirteen}>
-          <Image src="/thirteen.svg" alt="13" width={40} height={31} priority />
-        </div>
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://beta.nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={inter.className}>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p className={inter.className}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={inter.className}>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p className={inter.className}>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={inter.className}>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p className={inter.className}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+    </div>
+  );
 }
